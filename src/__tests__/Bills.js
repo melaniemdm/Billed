03af171080f4b986b4148639948649ba5f038b60
melaniemdm <melanieborja.pro@gmail.com>
2021-11-24 15:21:10 +0100

    feature(Bills.js) create 2tests-coverage 100% sur views/BillsUI

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,6 +1,28 @@
 import { screen } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
+import ErrorPage from "../views/ErrorPage.js";
+import LoadingPage from "../views/LoadingPage.js";
+
+jest.mock('../views/LoadingPage.js', () => {
+  const originalModule = jest.requireActual('../views/LoadingPage.js');
+  //Simule l'exportation par défaut et l'exportation nommée 'foo'
+  return {
+    __esModule: true,
+    ...originalModule,
+    default: jest.fn(() => 'LoadingPage'),
+     };
+});
+
+jest.mock('../views/ErrorPage.js', () => {
+  const originalModule = jest.requireActual('../views/ErrorPage.js');
+  //Simule l'exportation par défaut et l'exportation nommée 'foo'
+  return {
+    __esModule: true,
+    ...originalModule,
+    default: jest.fn(() => 'ErrorPage'),
+      };
+});
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -16,6 +38,20 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
+    })
+    test("then the loadingPage appears when loading is true", () => {
+      const loadingPageResult = LoadingPage();
+      const html = BillsUI({ data: bills, loading : true })
+      document.body.innerHTML = html
+      
+      expect(LoadingPage).toHaveBeenCalled();
+    })
+    test("then the ErrorPage appears when loading is false", () => {
+      const errorPageResult = ErrorPage();
+     const html = BillsUI({ data: bills, loading : false, error : true })
+      document.body.innerHTML = html
+     
+      expect(ErrorPage).toHaveBeenCalled();
     })
   })
 })
