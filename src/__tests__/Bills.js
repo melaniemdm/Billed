39ad0bfe0131b5532c4e1fb024ecf710f38e42a3
melaniemdm <melanieborja.pro@gmail.com>
2021-11-30 11:22:48 +0100

    fix(__test__/Bills.js): amelioration des tests

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,133 +1,142 @@
-import { screen } from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
+import { screen } from "@testing-library/dom";
+import BillsUI from "../views/BillsUI.js";
+import { bills } from "../fixtures/bills.js";
 import ErrorPage from "../views/ErrorPage.js";
 import LoadingPage from "../views/LoadingPage.js";
-import Bills from "../containers/Bills.js"
-import firestore from "../app/Firestore.js"
+import Bills from "../containers/Bills.js";
+import firestore from "../app/Firestore.js";
 
-jest.mock('../views/LoadingPage.js', () => {
-  const originalModule = jest.requireActual('../views/LoadingPage.js');
+jest.mock("../views/LoadingPage.js", () => {
+  const originalModule = jest.requireActual("../views/LoadingPage.js");
   //Simule l'exportation par défaut et l'exportation nommée 'foo'
   return {
     __esModule: true,
     ...originalModule,
-    default: jest.fn(() => 'LoadingPage'),
-     };
+    default: jest.fn(() => "LoadingPage"),
+  };
 });
 
-jest.mock('../views/ErrorPage.js', () => {
-  const originalModule = jest.requireActual('../views/ErrorPage.js');
+jest.mock("../views/ErrorPage.js", () => {
+  const originalModule = jest.requireActual("../views/ErrorPage.js");
   //Simule l'exportation par défaut et l'exportation nommée 'foo'
   return {
     __esModule: true,
     ...originalModule,
-    default: jest.fn(() => 'ErrorPage'),
-      };
+    default: jest.fn(() => "ErrorPage"),
+  };
 });
-
-
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: []})
-      document.body.innerHTML = html
+      const html = BillsUI({ data: [] });
+      document.body.innerHTML = html;
       //to-do write expect expression
-    })
+    });
     test("Then bills should be ordered from earliest to latest", () => {
-      const html = BillsUI({ data: bills })
-      document.body.innerHTML = html
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
-    })
+      const html = BillsUI({ data: bills });
+      document.body.innerHTML = html;
+      const dates = screen
+        .getAllByText(
+          /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
+        )
+        .map((a) => a.innerHTML);
+      const antiChrono = (a, b) => (a < b ? 1 : -1);
+      const datesSorted = [...dates].sort(antiChrono);
+      expect(dates).toEqual(datesSorted);
+    });
     test("then the loadingPage appears when loading is true", () => {
       const loadingPageResult = LoadingPage();
-      const html = BillsUI({ data: bills, loading : true })
-      document.body.innerHTML = html
-      
+      const html = BillsUI({ data: bills, loading: true });
+      document.body.innerHTML = html;
+
       expect(LoadingPage).toHaveBeenCalled();
-    })
+    });
     test("then the ErrorPage appears when loading is false", () => {
       const errorPageResult = ErrorPage();
-     const html = BillsUI({ data: bills, loading : false, error : true })
-      document.body.innerHTML = html
-     
+      const html = BillsUI({ data: bills, loading: false, error: true });
+      document.body.innerHTML = html;
+
       expect(ErrorPage).toHaveBeenCalled();
-    })
-  })
-})
+    });
+  });
+});
 
 describe("given i'm connected as employed'", () => {
   describe("when i create a Bills object", () => {
     //On teste que la création d'une liste de bill fonctionne => création d'un objet de classe Bills
     test("then it exists", () => {
-      const html = BillsUI({ data: bills})
-      document.body.innerHTML = html
-     let testBills = new Bills({ 
-       document: document, 
-       onNavigate: null,
-       firestore: null, 
-       localStorage: null })
-       let result = false;
-       if(testBills){
-      result = true;
-       }
-       expect(result).toBe(true)
-    })
+      const html = BillsUI({ data: bills });
+      document.body.innerHTML = html;
+      let testBills = new Bills({
+        document: document,
+        onNavigate: null,
+        firestore: null,
+        localStorage: null,
+      });
+      let result = false;
+      if (testBills) {
+        result = true;
+      }
+      expect(result).toBe(true);
+    });
     // on teste que lorsqu'un objet de classe Bills est créé, on peut cliquer sur l'oeil car il y a bien un évènement sur le noeud 'eye'
     test("then i can click on the eye", () => {
-      const html = BillsUI({ data: bills})
-      document.body.innerHTML = html
-     let testBills = new Bills({ 
-       document: document, 
-       onNavigate: null,
-       firestore: null, 
-       localStorage: null })
-     
-    $.fn.modal = jest.fn()
-      let nodeEye = document.querySelector('#eye')
-     nodeEye.click() 
-  expect($.fn.modal).toHaveBeenCalled()
-   
-    })
+      document.body.innerHTML = ` <div class="icon-actions">
+      <div id="eye" data-testid="icon-eye" data-bill-url="test.html">
+        eyeIcon
+      </div>
+    </div>`;
+      let testBills = new Bills({
+        document: document,
+        onNavigate: null,
+        firestore: null,
+        localStorage: null,
+      });
+
+      $.fn.modal = jest.fn();
+      let nodeEye = document.querySelector("#eye");
+      nodeEye.click();
+      expect($.fn.modal).toHaveBeenCalled();
+    });
     // on teste que l'on peut cliquer sur le bouton de création d'une nouvelle bill car il y a bien un évènement sur le bouton
     test("then i can click on the New Bills", () => {
-      const html = BillsUI({ data: bills})
-      document.body.innerHTML = html
-     let testBills = new Bills({ 
-       document: document, 
-       onNavigate: null,
-       firestore: null, 
-       localStorage: null })
-       testBills.onNavigate = jest.fn()
-       const buttonNewBill = document.querySelector(`button[data-testid="btn-new-bill"]`)
-       buttonNewBill.click() 
-  expect(testBills.onNavigate).toHaveBeenCalled()
-  
-    })
-    // on teste que l'on peut demander la liste des bills contenues dans l'objet de classe Bills en appelant la fonction getBills                                        
+      document.body.innerHTML = `<button type="button" data-testid='btn-new-bill' class="btn btn-primary">
+      Nouvelle note de frais</button>`;
+      let testBills = new Bills({
+        document: document,
+        onNavigate: null,
+        firestore: null,
+        localStorage: null,
+      });
+      testBills.onNavigate = jest.fn();
+      const buttonNewBill = document.querySelector(
+        `button[data-testid="btn-new-bill"]`
+      );
+      buttonNewBill.click();
+      expect(testBills.onNavigate).toHaveBeenCalled();
+    });
+    // on teste que l'on peut demander la liste des bills contenues dans l'objet de classe Bills en appelant la fonction getBills
     test("then i can get the bills", () => {
-      const html = BillsUI({ data: bills})
-      document.body.innerHTML = html
-  localStorage.__proto__.getItem=jest.fn((x) => '{"type":"Employee","email":"johndoe@email.com","password":"azerty","status":"connected"}')
-     let testBills = new Bills({ 
-       document: document, 
-       onNavigate: null,
-       firestore: null, 
-       localStorage:localStorage })
-       testBills.getBills()
-     expect(localStorage.getItem).toBeCalledWith("user")
-     localStorage.__proto__.getItem=jest.fn()
-     testBills.getBills()
-     expect(localStorage.getItem).toBeCalledWith("user")
-    
-      })
-
-      
-  })
-  
-  
-  })
+      const html = BillsUI({ data: bills });
+      document.body.innerHTML = html;
+      localStorage.__proto__.getItem = jest.fn(
+        (x) =>
+          '{"type":"Employee","email":"johndoe@email.com","password":"azerty","status":"connected"}'
+      );
+      let testBills = new Bills({
+        document: document,
+        onNavigate: null,
+        firestore: null,
+        localStorage: localStorage,
+      });
+      testBills.getBills();
+      expect(localStorage.getItem).toBeCalledWith("user");
+      localStorage.__proto__.getItem = jest.fn();
+      testBills.getBills();
+      expect(localStorage.getItem).toBeCalledWith("user");
+      let billRows = document.querySelectorAll("#eye") 
+     expect(billRows.length).toBe(4)
+    });
+  });
+});
